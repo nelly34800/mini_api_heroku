a6489377b9726fc7fcbf52e8d9ca5a424bff7ee5
nelly34800 <nellyboussekane@free.fr>
2026-07-04 15:52:25 +0200

    ajout CORS entêtes HTTP au backend pour que le nav autorise la connexion de l'api

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,4 +1,15 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Répond immédiatement aux requêtes préliminaires CORS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 require_once __DIR__ . '/../core/Router.php';
 
 $router = new Router();
--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,10 +1,12 @@
 const testApi =  document.getElementById("test-api");
+const API_URL =
+    "https://gentle-cove-89517-6b2a668603c5.herokuapp.com";
 
 testApi.addEventListener("click", testApiCall);
 
 async function testApiCall() {
     try {
-        const response = await fetch("/api/hello");
+        const response = await fetch(`${API_URL}/api/hello`);
         const data = await response.json();
         alert(data.message);
     } catch (error) {
